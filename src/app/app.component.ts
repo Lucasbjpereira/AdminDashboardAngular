092282d55d9d2cf22ad9cd6b1002d64f330d67a3
Lucas Pereira <lucas.pereira@modalgr.com.br>
2024-04-19 17:40:31 -0300

    add: criando o projeto e desenvolvendo a primeira estrutura do login

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, LoginComponent, FormsModule, ReactiveFormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'ecomadminpanel';
+}
